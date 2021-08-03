ceaf212b7f75db48bae561a5d739ae9a5aa4d82b
Kumari Mansi <mansinegiuk@gmail.com>
2021-08-03 19:21:32 +0530

    Week 3 problems done

---
+++ Week 3/Answer 2.cpp
@@ -0,0 +1,50 @@
+/* Q2
+Given an unsorted array of integers, design an algorithm and implement a program to sort this 
+array using selection sort. Your program should also find number of comparisons and number of 
+swaps required.
+*/
+#include <iostream>
+using namespace std;
+
+void Selection_Sort(int arr[], int n)
+{
+    int small, comp = 0, swaps = 0, i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+        small = i;
+        j = i - 1;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[small])
+                small = j;
+            comp++;
+        }
+        int temp = arr[i];
+        arr[i] = arr[small];
+        arr[small] = temp;
+        swaps++;
+    }
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\nComparisons=" << comp;
+    cout << "\nSwaps=" << swaps;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Selection_Sort(arr, n);
+    }
+    return 0;
+}
